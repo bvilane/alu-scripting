49079f418f5835d11fda24d3c1d2e1e8624c18fd
bvilane <b.vilane@alustudent.com>
2023-11-11 15:57:56 -0800

    final code

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,45 +1,43 @@
 #!/usr/bin/python3
 """ Module for a function that queries the Reddit API recursively."""
 
-
 import requests
 
+def count_words(subreddit, word_list, after='', word_dict=None):
+    """Prints a sorted count of given keywords."""
+    
+    if word_dict is None:
+        word_dict = {word.lower(): 0 for word in word_list}
 
-def count_words(subreddit, word_list, after='', word_dict={}):
-    """ prints a sorted count of given keywords """
-
-    if not word_dict:
-        for word in word_list:
-            if word.lower() not in word_dict:
-                word_dict[word.lower()] = 0
-
-    if after is None:
-        wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-        for word in wordict:
-            if word[1]:
-                print('{}: {}'.format(word[0], word[1]))
-        return None
+    if not after:
+        word_dict_sorted = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in word_dict_sorted:
+            if count:
+                print('{}: {}'.format(word, count))
+        return
 
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    header = {'user-agent': 'redquery'}
-    parameters = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=header, params=parameters,
-                            allow_redirects=False)
+    headers = {'user-agent': 'redquery'}
+    params = {'limit': 100, 'after': after}
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code != 200:
         return None
 
     try:
-        hot = response.json()['data']['children']
-        aft = response.json()['data']['after']
-        for post in hot:
-            title = post['data']['title']
-            lower = [word.lower() for word in title.split(' ')]
+        data = response.json()['data']
+        hot_posts = data.get('children', [])
+        after_next = data.get('after')
 
-            for word in word_dict.keys():
-                word_dict[word] += lower.count(word)
+        for post in hot_posts:
+            title = post['data'].get('title', '').lower()
+            for word in word_dict:
+                word_dict[word] += title.split().count(word)
 
-    except Exception:
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return None
 
-    count_words(subreddit, word_list, aft, word_dict)
+    count_words(subreddit, word_list, after_next, word_dict)
+
